4e72205b7e298e71725b286c6e4e3e52f56d3e61
MoizAli4444 <moizali1280@gmail.com>
2025-05-31 17:03:09 +0500

    Submitting Todo Items

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Todo = () => {
 
     const [todo,setTodo] = useState("");
+    const [todos,setTodos] = useState([]);
+
+    function handleSubmit(e){
+        e.preventDefault();
+        setTodos([...todos, todo]);
+        setTodo("");
+        
+    }
+
+    useEffect(() => {
+    console.log('Updated Todos:', todos);
+  }, [todos]); // This runs every time `todos` changes
 
   return (
     <div>
-        <form action="" method="post">
+        <form  onSubmit={handleSubmit}>
             <input onChange={(e)=> setTodo(e.target.value)} value={todo} type='text' />
             <button type='submit'>Add</button>
         </form>
+
     </div>
   )
 }
