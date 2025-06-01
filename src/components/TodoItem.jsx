a2cb8e33656454bc4a034080e005dd10194449e7
MoizAli4444 <moizali1280@gmail.com>
2025-06-01 23:05:28 +0500

    Add Delete button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -5,7 +5,9 @@ import styles from './todoitem.module.css'
 const TodoItem = ({item}) => {
   return (
     <div className={styles.item}>
-        <div className={styles.itemName} key={item}>{item}</div>
+        <div className={styles.itemName} key={item}>{item} 
+          <span> <button className={styles.deleteButton}>X</button></span>
+        </div>
         <hr className={styles.line} />
     </div>
   )
